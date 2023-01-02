e51b2cc3c66ca8cbca3e62483895645fa6a77c0f
branliang <bran.liang@shopify.com>
2023-01-02 11:14:33 +0800

    Add test for config len check

--- test/test_config.c
+++ test/test_config.c
@@ -14,6 +14,12 @@ void tearDown(void)
 
 void test_config_length_is_ok(void)
 {
+    // len = 50
+    char *max_string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    TEST_ASSERT_TRUE(he_internal_config_is_string_length_ok(max_string));
+    // len = 51
+    char *too_long_string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    TEST_ASSERT_FALSE(he_internal_config_is_string_length_ok(too_long_string));
 }
 
 #endif // TEST
